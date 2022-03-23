1c65debccb61ff92636cf8af8b56a4fd793e4cc1
kbelous <kbelous2022@gmail.com>
2022-03-23 21:39:13 +0300

    create Department class

--- data/__all_models.py
+++ data/__all_models.py
@@ -55,3 +55,15 @@ class Jobs(SqlAlchemyBase):
     end_date = sqlalchemy.Column(sqlalchemy.DateTime)
     is_finished = sqlalchemy.Column(sqlalchemy.Boolean)
     user = orm.relation('User')
+
+
+class Department(SqlAlchemyBase):
+    __tablename__ = 'jobs'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    chief = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    members = sqlalchemy.Column(sqlalchemy.String)
+    email = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    user = orm.relation('User')
